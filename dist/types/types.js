0478f3662b168c53408aeb7a20ae999691b6d2c2
Naishadh Patel <65918272+MrNNP@users.noreply.github.com>
2021-09-06 15:15:58 -0400

    add node and edge interface

--- dist/types/types.js
+++ dist/types/types.js
@@ -1,2 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var types;
+(function (types) {
+    var weightedNodeMap = /** @class */ (function () {
+        function weightedNodeMap() {
+        }
+        return weightedNodeMap;
+    }());
+    types.weightedNodeMap = weightedNodeMap;
+})(types || (types = {}));
+exports.default = types;
+//nodemap to add:
+/*
+TODO:make storage/way to maintain map in memory, preferable with least memory usage, also json compatible
+TODO:make way to add data to map
+TODO: add distance algorithum
+TODO: remove once done
+
+Brainstorming:
+
+seperate data into edges and points
+
+points will have a key, and data
+
+key will be and assigned number to the room, and a incremented number for hallway nodes
+
+data will include,
+whether its a room or hallway or locker, if room, then room number *string*. if locker, locker range array. if hallway, nothing
+the points coordinates on a map of school (not used at the moment but can be used in a visulization)
+
+edges will have a key, and 2 point keys, and a weight value
+
+
+units will be in meters
+
+
+*/ 
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,52 @@
 namespace types{
-    export namespace a{
-    
+    export class weightedNodeMap{
+        
+
+
     }
 
+    export interface node{
+        key:number;
+        nodeType:"room"|"hallway"|"locker",
+        roomData:string|Array<number>|undefined,
+        nodeLocation:Array<number>
+    }
 
-
+    export interface edge{
+        key:number;
+        connectedNodes:Array<number>
+        extraData?:String
+        weight:number
+    }
 
 
 }
 
 export default types;
+
+//nodemap to add:
+/*
+TODO:make storage/way to maintain map in memory, preferable with least memory usage, also json compatible
+TODO:make way to add data to map
+TODO: add distance algorithum
+TODO: remove once done
+
+Brainstorming:
+
+seperate data into edges and points
+
+points will have a key, and data
+
+key will be and assigned number to the room, and a incremented number for hallway nodes
+
+data will include, 
+whether its a room or hallway or locker, if room, then room number *string*. if locker, locker range array. if hallway, nothing
+the points coordinates on a map of school (not used at the moment but can be used in a visulization) 
+
+edges will have a key, and 2 point keys, and a weight value
+
+
+units will be in meters
+
+
+*/
